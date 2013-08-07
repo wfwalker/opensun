41fb14396b8c6566de15f477faae7323d935e9b2
Bill Walker <bwalker@mozilla.com>
2013-08-06 23:05:58 -0700

    don't need base url config

--- www/js/app.js
+++ www/js/app.js
@@ -2,12 +2,6 @@
 // The code below uses require.js, a module system for javscript:
 // http://requirejs.org/docs/api.html#define
 
-// Set the path to jQuery, which will fall back to the local version
-// if google is down
-require.config({
-    baseUrl: "js/lib",
-});
-
 // When you write javascript in separate files, list them as
 // dependencies along with jquery
 require([], function() {
