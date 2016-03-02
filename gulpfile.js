a7fd8ddc985fc59f5e04025f082476ce358518d4
Bill Walker <bwalker@mozilla.com>
2016-03-02 10:16:49 -0800

    update this project to use oghliner and a more sensible package structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,30 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var oghliner = require('oghliner');
 
-gulp.task('default', ['generate-service-worker']);
+gulp.task('default', ['offline']);
 
-gulp.task('generate-service-worker', function(callback) {
-  var path = require('path');
-  var swPrecache = require('sw-precache');
-  var rootDir = './';
-
-  swPrecache.write(path.join(rootDir, 'service-worker.js'), {
-    staticFileGlobs: [
-      rootDir + '{css,img,js}/*.{js,css,png,jpg,gif}',
-      rootDir + '{css,js}/lib/*.{js,css}',
-      rootDir + 'index\.html',
-      rootDir + 'l10n-data\.ini',
-      rootDir + 'favicon\.ico',
-      rootDir + 'w3c-manifest\.json',
-    ],
-    stripPrefix: rootDir
-  }, callback);
+gulp.task('build', function(callback) {
+  return gulp.src('app/**').pipe(gulp.dest('dist'));
 });
 
-gulp.task('serve', ['generate-service-worker'], function () {
+gulp.task('offline', ['build'], function() {
+  return oghliner.offline({
+    rootDir: 'dist/',
+    fileGlobs: [
+      '{css,img,js}/*.{js,css,png,jpg,gif}',
+      '{css,js}/lib/*.{js,css}',
+      'index\.html',
+      'l10n-data\.ini',
+      'favicon\.ico',
+      'w3c-manifest\.json',
+    ],
+  });
+});
+
+gulp.task('serve', ['offline'], function () {
   connect.server({
-    root: '.',
+    root: 'dist',
   });
 });
 
