d87d318f25883a26ea0be0da7c3f8bd58d53c23d
Bill Walker <bwalker@mozilla.com>
2016-03-02 14:11:59 -0800

    add lint task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var oghliner = require('oghliner');
+var eslint = require('gulp-eslint');
 
 gulp.task('default', ['offline']);
 
@@ -20,6 +21,17 @@ gulp.task('offline', ['build'], function() {
       'w3c-manifest\.json',
     ],
   });
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['app/js/{app,shotclock-draw,tests}.js']).pipe(eslint({
+    'rules':{
+        'quotes': [1, 'single'],
+        'semi': [1, 'always'],
+        'comma-dangle': [1, 'always-multiline'],
+        'quote-props': [1, 'as-needed']
+    }
+  })).pipe(eslint.format());
 });
 
 gulp.task('deploy', function() {
