6812fd40447b242840c4810d7aa58d9fd0cca48d
Bill Walker <bwalker@mozilla.com>
2016-03-16 08:41:57 -0700

    combine all the elements that rotate with sun angle into one container

--- app/js/shotclock-draw.js
+++ app/js/shotclock-draw.js
@@ -201,10 +201,8 @@ var shotclockDraw = {
             cssClass = lightRange[3];
         }
 
-        $('#shortsunangle')[0].transform.baseVal.getItem(0).setRotate(this.currentSunPosition.azimuth, 125, 125);
-
-        $('#shadow')[0].transform.baseVal.getItem(0).setRotate(this.currentSunPosition.azimuth, 125, 125);
-        $('#shortshadow')[0].transform.baseVal.getItem(0).setRotate(this.currentSunPosition.azimuth, 125, 125);
+        // rotate the collection of objects associated with the sun angle
+        $('#sunangle')[0].transform.baseVal.getItem(0).setRotate(this.currentSunPosition.azimuth, 125, 125);
 
         if (this.currentSunPosition.altitude > 0) { // if the sun is up
             if (this.currentSunPosition.altitude < 40) { // low sun, long shadow
